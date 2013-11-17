e8b087ceb0638e1a00a50471f30a0813386df479
Andrew Hodel <andrewhodel@gmail.com>
2013-11-17 18:27:32 -0200

    gh pages branch

---
+++ js/main.js
@@ -0,0 +1,307 @@
+TradeSocket.init();
+
+var cMap = {
+    'USD': [196, 112],
+    'EUR': [493, 89],
+    'JPY': [841, 132],
+    'CAD': [257, 82],
+    'GBP': [464, 86],
+    'CHF': [488, 102],
+    'RUB': [575, 84],
+    'AUD': [783, 291],
+    'SEK': [511, 66],
+    'DKK': [491, 78],
+    'HKD': [773, 166],
+    'PLN': [513, 83],
+    'CNY': [758, 131],
+    'SGD': [741, 230],
+    'THB': [736, 183],
+    'NOK': [490, 61],
+    'ILS': [562, 142],
+}
+
+var stage = new Kinetic.Stage({
+    container: 'mapcontainer',
+    width: 1015,
+    height: 530,
+});
+
+var mapLayer = new Kinetic.Layer({});
+var paisLayer = new Kinetic.Layer({});
+var graphLayer = new Kinetic.Layer({});
+
+var totalB = new Kinetic.Text({
+    x: 350,
+    y: 330,
+    text: '+0.00 BTC',
+    fontSize: 60,
+    fill: 'green'
+});
+
+for (var key in worldMap.shapes) {
+
+    var path = new Kinetic.Path({
+        data: worldMap.shapes[key],
+        fill: '#eee',
+        stroke: '#555',
+        strokeWidth: 1
+    });
+
+    mapLayer.add(path);
+}
+
+var cLabels = {};
+var cValueBoxes = {};
+var cValues = {};
+var bValueBoxes = {};
+var bValues = {};
+var rateValueBoxes = {};
+var xPositions = {};
+var tbValue = 0;
+
+var i = 0;
+for (var key in cMap) {
+
+    var stageWidth = stage.attrs.width;
+    var ybase = 400;
+
+    if (i > 15) {
+        ybase = 470;
+        xPositions[key] = (i - 16) * (stageWidth / Object.keys(cMap).length);
+    } else {
+        xPositions[key] = i * (stageWidth / Object.keys(cMap).length);
+    }
+
+    cLabels[key] = new Kinetic.Text({
+        x: xPositions[key],
+        y: ybase,
+        text: key,
+        fontStyle: 'bold',
+        fontSize: 24,
+        fill: 'red'
+    });
+
+    cValueBoxes[key] = new Kinetic.Text({
+        x: xPositions[key],
+        y: ybase + 40,
+        text: '',
+        fontSize: 12,
+        fill: 'red'
+    });
+
+    cValues[key] = 0;
+
+    bValueBoxes[key] = new Kinetic.Text({
+        x: xPositions[key],
+        y: ybase + 25,
+        text: '+0',
+        fontSize: 12,
+        fill: 'green'
+    });
+
+    bValues[key] = 0;
+
+    rateValueBoxes[key] = new Kinetic.Text({
+        x: xPositions[key],
+        y: ybase + 55,
+        text: '',
+        fontSize: 12,
+        fill: 'purple'
+    });
+
+    paisLayer.add(cLabels[key]);
+    paisLayer.add(totalB);
+    paisLayer.add(cValueBoxes[key]);
+    paisLayer.add(bValueBoxes[key]);
+    paisLayer.add(rateValueBoxes[key]);
+
+    i++;
+
+}
+
+stage.add(mapLayer);
+stage.add(paisLayer);
+stage.add(graphLayer);
+
+var img = new Image();
+img.src = "img/btc_logo_30.png";
+
+function aBuy(bitcoins, price, currencyName) {
+
+	//console.log('aBuy of '+bitcoins+' for $'+price+' of '+currencyName);
+
+    if (typeof cMap[currencyName] === 'undefined') {
+        return;
+    }
+
+    if ($('#collecting').is(':visible')) {
+        $('#collecting').hide();
+    }
+
+    var layer = new Kinetic.Layer();
+
+    var circle = new Kinetic.Circle({
+        radius: 10,
+        fill: '#f7931a',
+        stroke: '#ffffff',
+        strokeWidth: 2
+    });
+
+    var image = new Kinetic.Image({
+        image: img,
+        width: 30,
+        x: xPositions[currencyName] + 10,
+        y: 380,
+        height: 30,
+    });
+
+
+    layer.add(image);
+    stage.add(layer);
+
+    var tween = new Kinetic.Tween({
+        node: image,
+        duration: 6,
+        rotation: Math.PI * 2,
+        x: cMap[currencyName][0],
+        y: cMap[currencyName][1],
+        easing: Kinetic.Easings.EaseOut,
+        onFinish: function () {
+            layer.destroy();
+
+            var cVal = Number(bitcoins*price)+cValues[currencyName];
+            cValueBoxes[currencyName].setText(Math.round(cVal));
+            cValues[currencyName] = cVal;
+
+            var bVal = Number(bitcoins)+bValues[currencyName];
+            bValueBoxes[currencyName].setText('+' + Math.round(bVal));
+            bValues[currencyName] = bVal;
+
+            rateValueBoxes[currencyName].setText('@' + Math.round(Number(price)*100)/100);
+
+            var tbVal = Number(bitcoins) + tbValue;
+            totalB.setText('+' + Math.round(tbVal*100)/100 + ' BTC');
+            tbValue = tbVal;
+
+            nowBtcTotal += Number(bitcoins);
+
+            paisLayer.draw();
+
+        }
+    });
+
+    tween.play();
+
+}
+
+function getPosition(e) {
+
+    var targ;
+    if (!e)
+        e = window.event;
+    if (e.target)
+        targ = e.target;
+    else if (e.srcElement)
+        targ = e.srcElement;
+    if (targ.nodeType == 3) // defeat Safari bug
+        targ = targ.parentNode;
+
+    // jQuery normalizes the pageX and pageY
+    // pageX,Y are the mouse positions relative to the document
+    // offset() returns the position of the element relative to the document
+    var x = e.pageX - $(targ).offset().left;
+    var y = e.pageY - $(targ).offset().top;
+
+    return {
+        "x": x,
+        "y": y
+    };
+};
+
+$('canvas').click(function (event) {
+    // jQuery would normalize the event
+    position = getPosition(event);
+    //now you can use the x and y positions
+    console.log("X: " + position.x + " Y: " + position.y);
+});
+
+var nowBtcTotal = 0;
+var largest = 2;
+
+function tickGraph() {
+
+    if (nowBtcTotal > largest) {
+        largest = nowBtcTotal;
+    }
+
+    //console.log(graphLayer.getChildren().length + ' graph points');
+
+    if (graphLayer.getChildren().length > 0) {
+        // move graphLayer children left one px
+
+        for (var i = 0; i < graphLayer.getChildren().length; i++) {
+
+            var x = graphLayer.getChildren()[i].getX() - 4;
+            var y = graphLayer.getChildren()[i].getY();
+
+            if (x < 0) {
+                // remove it
+                graphLayer.getChildren()[i].destroy();
+            } else {
+                // add it
+                graphLayer.getChildren()[i].setPosition(x, y);
+            }
+
+        }
+
+    }
+
+    var calc = nowBtcTotal;
+    if (nowBtcTotal > largest) {
+        // set safe calc total
+        calc = largest;
+    }
+    var cx = 1000;
+    var cy = 389 - ((100 / largest) * calc);
+
+    if (cy < 379) {
+        console.log('add a high point number >10%');
+
+        var pn = Math.round(nowBtcTotal * 100) / 100;
+
+        var hp = new Kinetic.Text({
+            x: cx - 4,
+            y: cy - 10,
+            text: '+' + pn + ' BTC',
+            fontSize: 10,
+            fontStyle: 'bold',
+            fill: 'green'
+        });
+
+        hp.rotateDeg(-90);
+
+        graphLayer.add(hp);
+
+    }
+
+    var circle = new Kinetic.Circle({
+        radius: 2,
+        fill: '#f7931a',
+        x: cx,
+        y: cy
+    });
+
+    graphLayer.add(circle);
+    graphLayer.draw();
+
+    //console.log('adding tickGraph '+nowBtcTotal+', '+cx+','+cy);
+    nowBtcTotal = 0;
+
+}
+
+window.setInterval("tickGraph()", 1000);
+
+var socket = io.connect('http://fiatproxy1.jit.su:80');
+socket.on('proxyBuy', function (data) {
+    aBuy(data[0], data[1], data[2]);
+});
---
+++ js/mtgoxwebsocket.js
@@ -0,0 +1,81 @@
+/*
+
+socket code from MaxLaumeister, donate to him - 14zoTKB29NdsJRvk4qP3vB9mQZ3dcV3eWk
+
+https://github.com/MaxLaumeister/Listen-To-Bitcoin/blob/master/socket.js
+
+LICENSE
+
+If you distribute this project in part or in full, please attribute with a link to the GitHub page. This software is available under The MIT License, reproduced below.
+
+Copyright (c) 2013 Maximillian Laumeister
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+var satoshi = 100000000;
+
+function TradeSocket() {
+
+}
+
+TradeSocket.init = function() {
+	// Terminate previous connection, if any
+	if (this.connection)
+		this.connection.close();
+
+	if ('WebSocket' in window) {
+		var connection = new ReconnectingWebSocket('ws://websocket.mtgox.com:80/mtgox');
+		this.connection = connection;
+
+		connection.onopen = function() {
+			console.log('Mt.Gox: Connection open!');
+
+			var unsubDepth = {
+				"op" : "unsubscribe",
+				"channel" : "24e67e0d-1cad-4cc0-9e7a-f8523ef460fe"
+			}
+			connection.send(JSON.stringify(unsubDepth));
+
+		}
+
+		connection.onclose = function() {
+			console.log('Mt.Gox: Connection closed');
+		}
+
+		connection.onerror = function(error) {
+			console.log('Mt.Gox: Connection Error: ');
+			console.log(error);
+		}
+
+		connection.onmessage = function(e) {
+			var message = JSON.parse(e.data);
+			
+			if (message.trade) {
+
+				var bitcoins = message.trade.amount_int / satoshi;
+				var price = message.trade.price;
+				var currencyName = message.trade.price_currency;
+				
+				//console.log("Trade: " + bitcoins + " BTC | " + currency  + " " + currencyName);
+
+				aBuy(bitcoins, price, currencyName);
+
+			}
+
+		}
+	} else {
+		//WebSockets are not supported.
+		console.log("No websocket support.");
+	}
+}
+
+TradeSocket.close = function() {
+	if (this.connection)
+		this.connection.close();
+}
